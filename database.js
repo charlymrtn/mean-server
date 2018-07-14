21b18bca80088bdd84d946942e1156f66531b5b5
Carlos Martin <cjmar87@gmail.com>
2018-07-14 17:39:40 -0500

    server

---
+++ database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const URI = 'mongodb://charly_mrtn:9torres9@cluster1-shard-00-00-gluuy.mongodb.net:27017,cluster1-shard-00-01-gluuy.mongodb.net:27017,cluster1-shard-00-02-gluuy.mongodb.net:27017/mean-empleados?ssl=true&replicaSet=Cluster1-shard-0&authSource=admin';
+
+mongoose.connect(URI)
+    .then(db => console.log('db is connected'))
+    .catch(err => console.error(err));
+
+module.exports = mongoose;
